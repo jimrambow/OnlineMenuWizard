c3c326b339ad1ea3ba46573e52e0c53468c5d314
Jim Rambow <jimrambow@gmail.com>
2014-10-05 19:20:29 -0400

    updated views for menus via haml and JSON output, thinking of adding a CSV prinout of the menu...may take to much time

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,18 +1,20 @@
 class MenusController < ApplicationController
-  before_action :set_menu, only: [:show, :edit, :update, :destroy]
+ before_action :set_menu, only: [:show, :edit, :update, :destroy]
 
   def index
     @menus = Menu.all
-    respond_with(@menus)
+    
+    respond_to do |format|
+      format.html
+      format.csv { render text: @menus.export_to_csv }
+    end
   end
 
   def show
-    respond_with(@menu)
   end
 
   def new
     @menu = Menu.new
-    respond_with(@menu)
   end
 
   def edit
@@ -20,26 +22,47 @@ class MenusController < ApplicationController
 
   def create
     @menu = Menu.new(menu_params)
-    @menu.save
-    respond_with(@menu)
+
+    respond_to do |format|
+      if @menu.save
+        format.html { redirect_to @menu, notice: 'Menu was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @menu }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @menu.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @menu.update(menu_params)
-    respond_with(@menu)
+    respond_to do |format|
+      if @menu.update(menu_params)
+        format.html { redirect_to @menu, notice: 'Menu was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @menu.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @menu.destroy
-    respond_with(@menu)
+    respond_to do |format|
+      format.html { redirect_to menus_url }
+      format.json { head :no_content }
+    end
   end
 
   private
+
     def set_menu
       @menu = Menu.find(params[:id])
     end
 
     def menu_params
-      params.require(:menu).permit(:title, :description)
+      params.require(:menu).permit(:title, :description, :restaurant_id,
+           :items_attributes => [:id, :name, :description, :price, :_destroy]
+                                   )
     end
 end
